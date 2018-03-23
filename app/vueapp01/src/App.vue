3a967601d25f83afe92ae1800a6621c4a86b1e6b
mato <rmatiasgallardo@gmail.com>
2018-03-23 12:07:50 -0300

    metamask integration

--- app/vueapp01/src/App.vue
+++ app/vueapp01/src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <img src="./assets/rootstock.png">
     <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'RootStockWall'
 }
 </script>
 
